7cc43b7e9c6f92255029506690c72667569f4cfe
Nick Nicholas <opoudjis@optusnet.com.au>
2018-01-29 09:54:42 +1100

    hyphenated element names

---
+++ grammars/isodoc.make.sh
@@ -0,0 +1,8 @@
+if [[ ! -d jing-trang ]]; then
+  git clone https://github.com/relaxng/jing-trang.git
+fi
+cd jing-trang
+./ant
+cd ..
+java -jar jing-trang/build/trang.jar -I rnc -O rng isodoc.rnc isodoc.rng
+
