0b0e53f8e3592817d5d338f80062e9dcba298050
Nick Nicholas <opoudjis@optusnet.com.au>
2018-02-09 19:59:05 +1100

    Document model refactor

---
+++ grammars/copy.sh
@@ -0,0 +1,8 @@
+cp isodoc.rng ../../asciidoctor-iso/lib/asciidoctor/iso
+cp isostandard.rng ../../asciidoctor-iso/lib/asciidoctor/iso
+cp isodoc.rng ../../asciidoctor-gb/lib/asciidoctor/gb
+cat isostandard.rng | ruby -pe '$_.gsub!(/<grammar ns=\S+/, "<grammar") ' >  ../../asciidoctor-gb/lib/asciidoctor/gb/isostandard.rng
+cp gbstandard.rng ../../asciidoctor-gb/lib/asciidoctor/gb
+cp isodoc.rng ../../asciidoctor-csd/lib/asciidoctor/csd
+cat isostandard.rng | ruby -pe '$_.gsub!(/<grammar ns=\S+/, "<grammar") ' >  ../../asciidoctor-csd/lib/asciidoctor/csd/isostandard.rng
+cp csd.rng ../../asciidoctor-csd/lib/asciidoctor/csd
