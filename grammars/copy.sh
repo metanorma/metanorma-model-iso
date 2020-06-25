4e60ce721e31ae4e34caf64b56e2f3d545b841da
Nick Nicholas <opoudjis@gmail.com>
2020-06-25 23:27:35 +1000

    update gem names: https://github.com/metanorma/metanorma/issues/102

--- grammars/copy.sh
+++ grammars/copy.sh
@@ -20,41 +20,36 @@ cp biblio.rng ../../metanorma-gb/lib/asciidoctor/gb
 cp isodoc.rng ../../metanorma-gb/lib/asciidoctor/gb
 cp isostandard.rng ../../metanorma-gb/lib/asciidoctor/gb/isostandard.rng
 cat gbstandard.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/gb" }) '>  ../../metanorma-gb/lib/asciidoctor/gb/gbstandard.rng
-cp basicdoc.rng ../../metanorma-csd/lib/asciidoctor/csd
-cp reqt.rng ../../metanorma-csd/lib/asciidoctor/csd
-cp biblio.rng ../../metanorma-csd/lib/asciidoctor/csd
-cp isodoc.rng ../../metanorma-csd/lib/asciidoctor/csd
-cat csd.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/csd" }) '>  ../../metanorma-csd/lib/asciidoctor/csd/csd.rng
+cp basicdoc.rng ../../metanorma-csd/lib/asciidoctor/cc
+cp reqt.rng ../../metanorma-csd/lib/asciidoctor/cc
+cp biblio.rng ../../metanorma-csd/lib/asciidoctor/cc
+cp isodoc.rng ../../metanorma-csd/lib/asciidoctor/cc
+cat csd.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/csd" }) '>  ../../metanorma-csd/lib/asciidoctor/cc/cc.rng
 cp basicdoc.rng ../../metanorma-csand/lib/asciidoctor/csa
 cp reqt.rng ../../metanorma-csand/lib/asciidoctor/csa
 cp biblio.rng ../../metanorma-csand/lib/asciidoctor/csa
 cp isodoc.rng ../../metanorma-csand/lib/asciidoctor/csa
 cat csa.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/csa" }) '>  ../../metanorma-csand/lib/asciidoctor/csa/csa.rng
-cp basicdoc.rng ../../metanorma-m3d/lib/asciidoctor/m3d
-cp reqt.rng ../../metanorma-m3d/lib/asciidoctor/m3d
-cp biblio.rng ../../metanorma-m3d/lib/asciidoctor/m3d
-cp isodoc.rng ../../metanorma-m3d/lib/asciidoctor/m3d
-cat m3d.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/m3d" }) '>  ../../metanorma-m3d/lib/asciidoctor/m3d/m3d.rng
-cp basicdoc.rng ../../metanorma-rsd/lib/asciidoctor/rsd
-cp reqt.rng ../../metanorma-rsd/lib/asciidoctor/rsd
-cp biblio.rng ../../metanorma-rsd/lib/asciidoctor/rsd
-cp isodoc.rng ../../metanorma-rsd/lib/asciidoctor/rsd
-cat rsd.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/rsd" }) '>  ../../metanorma-rsd/lib/asciidoctor/rsd/rsd.rng
-cp basicdoc.rng ../../metanorma-sample/lib/asciidoctor/sample
-cp reqt.rng ../../metanorma-sample/lib/asciidoctor/sample
-cp biblio.rng ../../metanorma-sample/lib/asciidoctor/sample
-cp isodoc.rng ../../metanorma-sample/lib/asciidoctor/sample
-cat rsd.rng | ruby -pe '$_.gsub!(/rsd-standard/, "sample-standard") ' | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/sample" }) '  > ../../metanorma-sample/lib/asciidoctor/sample/sample.rng
+cp basicdoc.rng ../../metanorma-m3d/lib/asciidoctor/m3aawg
+cp reqt.rng ../../metanorma-m3d/lib/asciidoctor/m3aawg
+cp biblio.rng ../../metanorma-m3d/lib/asciidoctor/m3aawg
+cp isodoc.rng ../../metanorma-m3d/lib/asciidoctor/m3aawg
+cat m3d.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/m3d" }) '>  ../../metanorma-m3d/lib/asciidoctor/m3aawg/m3d.rng
+cp basicdoc.rng ../../metanorma-rsd/lib/asciidoctor/ribose
+cp reqt.rng ../../metanorma-rsd/lib/asciidoctor/ribose
+cp biblio.rng ../../metanorma-rsd/lib/asciidoctor/ribose
+cp isodoc.rng ../../metanorma-rsd/lib/asciidoctor/ribose
+cat rsd.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/rsd" }) '>  ../../metanorma-rsd/lib/asciidoctor/ribose/rsd.rng
 cp basicdoc.rng ../../metanorma-acme/lib/asciidoctor/generic
 cp reqt.rng ../../metanorma-acme/lib/asciidoctor/generic
 cp biblio.rng ../../metanorma-acme/lib/asciidoctor/generic
 cp isodoc.rng ../../metanorma-acme/lib/asciidoctor/generic
 cat rsd.rng | ruby -pe '$_.gsub!(/rsd-standard/, "generic-standard") ' | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/generic" }) ' > ../../metanorma-acme/lib/asciidoctor/generic/generic.rng
-cp basicdoc.rng ../../metanorma-mpfd/lib/asciidoctor/mpfd
-cp reqt.rng ../../metanorma-mpfd/lib/asciidoctor/mpfd
-cp biblio.rng ../../metanorma-mpfd/lib/asciidoctor/mpfd
-cp isodoc.rng ../../metanorma-mpfd/lib/asciidoctor/mpfd
-cat mpfd.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/mpfd" }) '>  ../../metanorma-mpfd/lib/asciidoctor/mpfd/mpfd.rng
+cp basicdoc.rng ../../metanorma-mpfd/lib/asciidoctor/mpfa
+cp reqt.rng ../../metanorma-mpfd/lib/asciidoctor/mpfa
+cp biblio.rng ../../metanorma-mpfd/lib/asciidoctor/mpfa
+cp isodoc.rng ../../metanorma-mpfd/lib/asciidoctor/mpfa
+cat mpfd.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/mpfd" }) '>  ../../metanorma-mpfd/lib/asciidoctor/mpfa/mpfd.rng
 cp basicdoc.rng ../../metanorma-unece/lib/asciidoctor/un
 cp reqt.rng ../../metanorma-unece/lib/asciidoctor/un
 cp biblio.rng ../../metanorma-unece/lib/asciidoctor/un
@@ -80,11 +75,11 @@ cp reqt.rng ../../metanorma-itu/lib/asciidoctor/itu
 cp biblio.rng ../../metanorma-itu/lib/asciidoctor/itu
 cp isodoc.rng ../../metanorma-itu/lib/asciidoctor/itu
 cat itu.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/itu" }) '>  ../../metanorma-itu/lib/asciidoctor/itu/itu.rng
-cp basicdoc.rng ../../metanorma-vsd/lib/asciidoctor/vsd
-cp reqt.rng ../../metanorma-vsd/lib/asciidoctor/vsd
-cp biblio.rng ../../metanorma-vsd/lib/asciidoctor/vsd
-cp isodoc.rng ../../metanorma-vsd/lib/asciidoctor/vsd
-cat rsd.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/vsd" }) '>  ../../metanorma-vsd/lib/asciidoctor/vsd/vsd.rng
+cp basicdoc.rng ../../metanorma-vsd/lib/asciidoctor/vg
+cp reqt.rng ../../metanorma-vsd/lib/asciidoctor/vg
+cp biblio.rng ../../metanorma-vsd/lib/asciidoctor/vg
+cp isodoc.rng ../../metanorma-vsd/lib/asciidoctor/vg
+cat rsd.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/vsd" }) '>  ../../metanorma-vsd/lib/asciidoctor/vg/vsd.rng
 cp basicdoc.rng ../../metanorma-iho/lib/asciidoctor/iho
 cp reqt.rng ../../metanorma-iho/lib/asciidoctor/iho
 cp biblio.rng ../../metanorma-iho/lib/asciidoctor/iho
