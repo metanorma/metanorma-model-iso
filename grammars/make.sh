0f481a295d2f4471b0c5995cf38835a9380faf5d
Nick Nicholas <opoudjis@gmail.com>
2022-11-25 21:48:22 +1100

    https://github.com/metanorma/metanorma-model-iso/issues/75

--- grammars/make.sh
+++ grammars/make.sh
@@ -28,7 +28,7 @@ echo "\"metanorma-requirements-models\": \"$var\"," >> ../../versions.json
 cd ../..
 cp metanorma-requirements-models/grammars/reqt.rnc .
 
-for i in ieee iso iec bsi gb mpfa bipm w3c 3gpp csa cc ietf iho itu m3aawg nist ribose ogc un cen
+for i in ieee iso iec bsi gb mpfa bipm w3c 3gpp csa cc ietf iho itu m3aawg nist ribose ogc un cen ecma cie iana omg
 do
   cd relaton-model-$i/grammars
   git checkout main && git pull
@@ -55,7 +55,7 @@ fi
 
 echo "Compiling..."
 
-for i in biblio biblio-compile biblio-standoc basicdoc reqt relaton-ieee relaton-iso relaton-iec relaton-bsi relaton-gb relaton-mpfa relaton-bipm relaton-w3c relaton-3gpp relaton-csa relaton-cc relaton-ietf relaton-iho relaton-itu relaton-m3aawg relaton-nist relaton-ribose relaton-ogc relaton-un relaton-cen isodoc isodoc-compile isostandard isostandard-compile isostandard-amd iec csd csa gbstandard m3d rsd ieee un ogc nist itu ietf iho bipm bsi relaton-ieee-compile relaton-iso-compile relaton-iec-compile relaton-bsi-compile relaton-gb-compile relaton-mpfa-compile relaton-bipm-compile relaton-w3c-compile relaton-3gpp-compile relaton-csa-compile relaton-cc-compile relaton-ietf-compile relaton-iho-compile relaton-itu-compile relaton-m3aawg-compile relaton-nist-compile relaton-ribose-compile relaton-ogc-compile relaton-un-compile relaton-cen-compile
+for i in biblio biblio-compile biblio-standoc basicdoc reqt relaton-ieee relaton-iso relaton-iec relaton-bsi relaton-gb relaton-mpfa relaton-bipm relaton-w3c relaton-3gpp relaton-csa relaton-cc relaton-ietf relaton-iho relaton-itu relaton-m3aawg relaton-nist relaton-ribose relaton-ogc relaton-un relaton-cen relaton-ecma relaton-cie relaton-iana relaton-omg isodoc isodoc-compile isostandard isostandard-compile isostandard-amd iec csd csa gbstandard m3d rsd ieee un ogc nist itu ietf iho bipm bsi relaton-ieee-compile relaton-iso-compile relaton-iec-compile relaton-bsi-compile relaton-gb-compile relaton-mpfa-compile relaton-bipm-compile relaton-w3c-compile relaton-3gpp-compile relaton-csa-compile relaton-cc-compile relaton-ietf-compile relaton-iho-compile relaton-itu-compile relaton-m3aawg-compile relaton-nist-compile relaton-ribose-compile relaton-ogc-compile relaton-un-compile relaton-cen-compile relaton-ecma-compile relaton-cie-compile relaton-iana-compile relaton-omg-compile
 do
   echo $i
   java -jar jing-trang/build/trang.jar -I rnc -O rng $i.rnc $i.rng
