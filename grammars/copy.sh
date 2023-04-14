bcd43e484dfc53d063c40133c6f03cb9b38a55ba
Nick Nicholas <opoudjis@gmail.com>
2023-04-14 13:26:01 +0800

    https://github.com/metanorma/metanorma-standoc/issues/772

--- grammars/copy.sh
+++ grammars/copy.sh
@@ -1,72 +1,31 @@
 echo "Copying..."
 
-cp basicdoc.rng reqt.rng biblio.rng biblio-standoc.rng isodoc.rng ../../metanorma-standoc/lib/metanorma/standoc
+metanorma_version=`git tag --sort=committerdate | tail -1`
+
+for i in standoc iso iec bsi jis cc csa ribose generic ieee un ogc nist ietf itu iho bipm
+do
+  cat isodoc.rng | ruby -pe "\$_.gsub!(/<grammar/, %{<!-- VERSION ${metanorma_version} -->\n<grammar}) " > ../../metanorma-$i/lib/metanorma/$i/isodoc.rng
+  cp basicdoc.rng reqt.rng biblio.rng biblio-standoc.rng ../../metanorma-$i/lib/metanorma/$i
+done
+
+for i in iso iec bsi jis cc csa ribose ieee un ogc nist ietf itu iho bipm
+do
+  cp relaton-$i.rng ../../metanorma-$i/lib/metanorma/$i
+done
+
+for i in iso iec bsi jis
+do
+cp isostandard.rng ../../metanorma-$i/lib/metanorma/$i
+done
+
+for i in iec bsi jis cc csa ribose ieee un ogc nist ietf itu iho bipm generic
+do
+cat $i.rng | ruby -pe "\$_.gsub!(/<grammar /, %{<grammar ns='https://www.metanorma.org/ns/${i}' }) ">  ../../metanorma-$i/lib/metanorma/$i/$i.rng
+done
+
 cat isodoc-compile.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/standoc" }) '> ../../metanorma-standoc/lib/metanorma/standoc/isodoc-compile.rng
 
-cp basicdoc.rng reqt.rng biblio.rng biblio-standoc.rng isodoc.rng ../../metanorma-iso/lib/metanorma/iso
-cp relaton-iso.rng ../../metanorma-iso/lib/metanorma/iso
-cp isostandard.rng ../../metanorma-iso/lib/metanorma/iso
 cat isostandard-compile.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/iso" }) '>  ../../metanorma-iso/lib/metanorma/iso/isostandard-compile.rng
 cat isostandard-amd.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/iso" }) '>  ../../metanorma-iso/lib/metanorma/iso/isostandard-amd.rng
 
-cp basicdoc.rng reqt.rng biblio.rng biblio-standoc.rng isodoc.rng ../../metanorma-iec/lib/metanorma/iec
-cp relaton-iec.rng ../../metanorma-iec/lib/metanorma/iec
-cp isostandard.rng ../../metanorma-iec/lib/metanorma/iec/isostandard.rng
-cat iec.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/iec" }) '>  ../../metanorma-iec/lib/metanorma/iec/iec.rng
 
-cp basicdoc.rng reqt.rng biblio.rng biblio-standoc.rng isodoc.rng ../../metanorma-bsi/lib/metanorma/bsi
-cp relaton-bsi.rng ../../metanorma-bsi/lib/metanorma/bsi
-cp isostandard.rng ../../metanorma-bsi/lib/metanorma/bsi/isostandard.rng
-cat bsi.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/bsi" }) '>  ../../metanorma-bsi/lib/metanorma/bsi/bsi.rng
-
-cp basicdoc.rng reqt.rng biblio.rng biblio-standoc.rng isodoc.rng ../../metanorma-jis/lib/metanorma/jis
-cp relaton-jis.rng ../../metanorma-jis/lib/metanorma/jis
-cp isostandard.rng ../../metanorma-jis/lib/metanorma/jis/isostandard.rng
-cat jis.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/jis" }) '>  ../../metanorma-jis/lib/metanorma/jis/jis.rng
-
-cp basicdoc.rng reqt.rng biblio.rng biblio-standoc.rng isodoc.rng ../../metanorma-csd/lib/metanorma/cc
-cp relaton-cc.rng ../../metanorma-csd/lib/metanorma/cc
-cat csd.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/csd" }) '>  ../../metanorma-csd/lib/metanorma/cc/cc.rng
-
-cp basicdoc.rng reqt.rng biblio.rng biblio-standoc.rng isodoc.rng ../../metanorma-csand/lib/metanorma/csa
-cp relaton-csa.rng ../../metanorma-csand/lib/metanorma/csa
-cat csa.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/csa" }) '>  ../../metanorma-csand/lib/metanorma/csa/csa.rng
-
-cp basicdoc.rng reqt.rng biblio.rng biblio-standoc.rng isodoc.rng ../../metanorma-rsd/lib/metanorma/ribose
-cp relaton-ribose.rng ../../metanorma-rsd/lib/metanorma/ribose
-cat rsd.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/rsd" }) '>  ../../metanorma-rsd/lib/metanorma/ribose/rsd.rng
-
-cp basicdoc.rng reqt.rng biblio.rng biblio-standoc.rng isodoc.rng ../../metanorma-acme/lib/metanorma/generic
-cat isodoc-compile.rng | ruby -pe '$_.gsub!(/rsd-standard/, "generic-standard") ' | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/generic" }) ' > ../../metanorma-acme/lib/metanorma/generic/generic.rng
-
-cp basicdoc.rng reqt.rng biblio.rng biblio-standoc.rng isodoc.rng ../../metanorma-ieee/lib/metanorma/ieee
-cp relaton-ieee.rng ../../metanorma-ieee/lib/metanorma/ieee
-cat ieee.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/ieee" }) '>  ../../metanorma-ieee/lib/metanorma/ieee/ieee.rng
-
-cp basicdoc.rng reqt.rng biblio.rng biblio-standoc.rng isodoc.rng ../../metanorma-unece/lib/metanorma/un
-cp relaton-un.rng ../../metanorma-unece/lib/metanorma/un
-cat un.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/un" }) '>  ../../metanorma-unece/lib/metanorma/un/un.rng
-
-cp basicdoc.rng reqt.rng biblio.rng biblio-standoc.rng isodoc.rng ../../metanorma-ogc/lib/metanorma/ogc
-cp relaton-ogc.rng ../../metanorma-ogc/lib/metanorma/ogc
-cat ogc.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/ogc" }) '>  ../../metanorma-ogc/lib/metanorma/ogc/ogc.rng
-
-cp basicdoc.rng reqt.rng biblio.rng biblio-standoc.rng isodoc.rng ../../metanorma-nist/lib/metanorma/nist
-cp relaton-nist.rng ../../metanorma-nist/lib/metanorma/nist
-cat nist.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/nist" }) '>  ../../metanorma-nist/lib/metanorma/nist/nist.rng
-
-cp basicdoc.rng reqt.rng biblio.rng biblio-standoc.rng isodoc.rng ../../metanorma-ietf/lib/metanorma/ietf
-cp relaton-ietf.rng ../../metanorma-ietf/lib/metanorma/ietf
-cat ietf.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/ietf" }) '>  ../../metanorma-ietf/lib/metanorma/ietf/ietf.rng
-
-cp basicdoc.rng reqt.rng biblio.rng biblio-standoc.rng isodoc.rng ../../metanorma-itu/lib/metanorma/itu
-cp relaton-itu.rng ../../metanorma-itu/lib/metanorma/itu
-cat itu.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/itu" }) '>  ../../metanorma-itu/lib/metanorma/itu/itu.rng
-
-cp basicdoc.rng reqt.rng biblio.rng biblio-standoc.rng isodoc.rng ../../metanorma-iho/lib/metanorma/iho
-cp relaton-iho.rng ../../metanorma-iho/lib/metanorma/iho
-cat iho.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/iho" }) '>  ../../metanorma-iho/lib/metanorma/iho/iho.rng
-
-cp basicdoc.rng reqt.rng biblio.rng biblio-standoc.rng isodoc.rng ../../metanorma-bipm/lib/metanorma/bipm
-cp relaton-bipm.rng ../../metanorma-bipm/lib/metanorma/bipm
-cat bipm.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/bipm" }) '>  ../../metanorma-bipm/lib/metanorma/bipm/bipm.rng
