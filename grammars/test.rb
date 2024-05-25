b641a73fa77d7766f6ba37fc4e2529d861e0c434
Nick Nicholas <opoudjis@gmail.com>
2024-05-26 02:21:36 +1000

    add relaton-plateau: https://github.com/metanorma/metanorma-plateau/issues/6

--- grammars/test.rb
+++ grammars/test.rb
@@ -1,19 +1,21 @@
 require "jing"
 
 ret = ""
-GRAMMARS = %w(biblio-compile isodoc-compile isostandard-compile isostandard-amd
-              iec generic csd csa gbstandard m3d rsd ieee un ogc nist itu ietf
-              iho bipm bsi jis plateau
-              relaton-ieee-compile relaton-iso-compile relaton-iec-compile
-              relaton-bsi-compile relaton-gb-compile relaton-mpfa-compile
-              relaton-bipm-compile relaton-w3c-compile relaton-3gpp-compile
-              relaton-csa-compile relaton-cc-compile relaton-ietf-compile
-              relaton-iho-compile relaton-itu-compile relaton-m3aawg-compile
-              relaton-nist-compile relaton-ribose-compile relaton-ogc-compile
-              relaton-un-compile relaton-cen-compile relaton-ecma-compile
-              relaton-cie-compile relaton-iana-compile relaton-omg-compile
-              relaton-oasis-compile relaton-jis-compile relaton-etsi-compile)
-  .freeze
+GRAMMARS =
+  %w(biblio-compile isodoc-compile isostandard-compile isostandard-amd
+     iec generic csd csa gbstandard m3d rsd ieee un ogc nist itu ietf
+     iho bipm bsi jis plateau
+     relaton-ieee-compile relaton-iso-compile relaton-iec-compile
+     relaton-bsi-compile relaton-gb-compile relaton-mpfa-compile
+     relaton-bipm-compile relaton-w3c-compile relaton-3gpp-compile
+     relaton-csa-compile relaton-cc-compile relaton-ietf-compile
+     relaton-iho-compile relaton-itu-compile relaton-m3aawg-compile
+     relaton-nist-compile relaton-ribose-compile relaton-ogc-compile
+     relaton-un-compile relaton-cen-compile relaton-ecma-compile
+     relaton-cie-compile relaton-iana-compile relaton-omg-compile
+     relaton-oasis-compile relaton-jis-compile relaton-etsi-compile
+     relaton-plateau-compile)
+    .freeze
 
 GRAMMARS.each do |g|
   warn "validating #{g}"
