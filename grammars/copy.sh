1bedd0ee2ac472eb1979c3e04826fb3895788442
Nick Nicholas <opoudjis@gmail.com>
2025-10-01 23:38:46 +1000

    remove distinct CSA grammar:https://github.com/metanorma/metanorma-csa/issues/327

--- grammars/copy.sh
+++ grammars/copy.sh
@@ -2,13 +2,13 @@ echo "Copying..."
 
 metanorma_version=`git tag --sort=committerdate | tail -1`
 
-for i in standoc iso iec bsi jis plateau cc csa ribose generic ieee ogc nist ietf itu iho bipm
+for i in standoc iso iec bsi jis plateau cc ribose generic ieee ogc nist ietf itu iho bipm
 do
   cat isodoc.rng | ruby -pe "\$_.gsub!(/(<grammar[^>]+>)/, %{\\\\1\n<!-- VERSION ${metanorma_version} -->\n}) " > ../../metanorma-$i/lib/metanorma/$i/isodoc.rng
   cp basicdoc.rng reqt.rng biblio.rng biblio-standoc.rng ../../metanorma-$i/lib/metanorma/$i
 done
 
-for i in iso iec bsi jis cc csa ribose ieee ogc nist ietf itu iho bipm
+for i in iso iec bsi jis cc ribose ieee ogc nist ietf itu iho bipm
 do
   cp relaton-$i.rng ../../metanorma-$i/lib/metanorma/$i
 done
@@ -21,7 +21,7 @@ do
 cp isostandard.rng ../../metanorma-$i/lib/metanorma/$i
 done
 
-for i in iec bsi jis cc csa ribose ieee ogc nist ietf itu iho bipm generic plateau
+for i in iec bsi jis cc ribose ieee ogc nist ietf itu iho bipm generic plateau
 do
 cat $i.rng | ruby -pe "\$_.gsub!(/<grammar /, %{<grammar ns='https://www.metanorma.org/ns/standoc' }) ">  ../../metanorma-$i/lib/metanorma/$i/$i.rng
 done
