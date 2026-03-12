96e7458a235545de8292a8cd2e32b36bbded932b
Nick Nicholas <opoudjis@gmail.com>
2026-03-12 11:38:17 +1100

    Remove editorialgroup from biblio-presentation

--- grammars/copy.sh
+++ grammars/copy.sh
@@ -2,7 +2,10 @@ echo "Copying..."
 
 metanorma_version=`git tag --sort=committerdate | tail -1`
 
-for i in standoc iso iec bsi jis plateau cc ribose generic ieee ogc nist ietf itu iho bipm
+cat isodoc.rng | ruby -pe "\$_.gsub!(/(<grammar[^>]+>)/, %{\\\\1\n<!-- VERSION ${metanorma_version} -->\n}) " > ../../metanorma-standoc/lib/metanorma/validate/isodoc.rng
+cp basicdoc.rng reqt.rng biblio.rng biblio-standoc.rng ../../metanorma-standoc/lib/metanorma/validate
+
+for i in iso iec bsi jis plateau cc ribose generic ieee ogc nist ietf itu iho bipm
 do
   cat isodoc.rng | ruby -pe "\$_.gsub!(/(<grammar[^>]+>)/, %{\\\\1\n<!-- VERSION ${metanorma_version} -->\n}) " > ../../metanorma-$i/lib/metanorma/$i/isodoc.rng
   cp basicdoc.rng reqt.rng biblio.rng biblio-standoc.rng ../../metanorma-$i/lib/metanorma/$i
@@ -26,8 +29,7 @@ do
 cat $i.rng | ruby -pe "\$_.gsub!(/<grammar /, %{<grammar ns='https://www.metanorma.org/ns/standoc' }) ">  ../../metanorma-$i/lib/metanorma/$i/$i.rng
 done
 
-cat isodoc-compile.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/standoc" }) '> ../../metanorma-standoc/lib/metanorma/standoc/isodoc-compile.rng
-
+cat isodoc-compile.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/standoc" }) '> ../../metanorma-standoc/lib/metanorma/validate/isodoc-compile.rng
 cat isostandard-compile.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/standoc" }) '>  ../../metanorma-iso/lib/metanorma/iso/isostandard-compile.rng
 cat isostandard-amd.rng | ruby -pe '$_.gsub!(/<grammar /, %{<grammar ns="https://www.metanorma.org/ns/standoc" }) '>  ../../metanorma-iso/lib/metanorma/iso/isostandard-amd.rng
 
